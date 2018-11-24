c9d6260266373a12cc4882ad8288d463eaca5aa3
cahmed <17.chayon@gmail.com>
2018-11-24 17:26:12 +0600

    project init
    add main activity and wrtite tweet activity and adaptar for the showing the tweet

--- app/src/main/java/app/zalora/com/zaloraassignment/MainActivity.kt
+++ app/src/main/java/app/zalora/com/zaloraassignment/MainActivity.kt
@@ -1,14 +1,33 @@
-package app.zalora.com.zaloraassignment
+package app.zalora.com.zalora
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.util.Log.e
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 
+import butterknife.BindView
+import com.mathi.recyclerviewsample.CustomAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
+import android.support.v4.app.NavUtils
+import app.zalora.com.zaloraassignment.R
+import app.zalora.com.zaloraassignment.TweetEntity
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity()  {
+
+    internal var dataList: ArrayList<TweetEntity>? = null
+    internal var customAdapter: CustomAdapter? = null
+    internal var RESULT_CODE = 5;
+    @BindView(R.id.recycler_view) @JvmField var androidRecyclerView: RecyclerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +37,18 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+            var intent = Intent(this, WriteTweetActivity::class.java)
+            startActivityForResult(intent, RESULT_CODE)
         }
+        dataList = ArrayList<TweetEntity>()
+    }
+
+    private fun setupAdaptar() {
+        customAdapter = CustomAdapter(this@MainActivity, dataList!!)
+        val mLayoutManager = LinearLayoutManager(applicationContext)
+        androidRecyclerView?.layoutManager = mLayoutManager;
+       androidRecyclerView!!.layoutManager = mLayoutManager
+        androidRecyclerView?.adapter = customAdapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -32,8 +62,25 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
+            android.R.id.home -> {
+               finish()
+                return true
+            }
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == RESULT_CODE) {
+                var resultString: String = data!!.getStringExtra("tweet-data")
+                e("message ",resultString);
+            }
+        }
+    }
+
+
+
 }
